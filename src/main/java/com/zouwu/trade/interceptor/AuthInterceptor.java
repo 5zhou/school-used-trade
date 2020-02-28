14569ac4a92eaff487df7aaf73f44f47fec5268a
zouwu <zouwu1995@gmail.com>
2020-02-29 02:48:20 +0800

    更新未登录时的提示信息

--- src/main/java/com/zouwu/trade/interceptor/AuthInterceptor.java
+++ src/main/java/com/zouwu/trade/interceptor/AuthInterceptor.java
@@ -1,5 +1,8 @@
 package com.zouwu.trade.interceptor;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.zouwu.trade.dto.ApiResponse;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.servlet.HandlerInterceptor;
@@ -7,9 +10,12 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.nio.charset.StandardCharsets;
 
 @Component
 public class AuthInterceptor implements HandlerInterceptor {
+
+    private static ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
@@ -18,6 +24,8 @@ public class AuthInterceptor implements HandlerInterceptor {
             return true;
         } else {
             response.setStatus(401);
+            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+            response.getOutputStream().write(objectMapper.writeValueAsString(new ApiResponse<String>(401, "未登录", null)).getBytes(StandardCharsets.UTF_8));
             return false;
         }
     }
